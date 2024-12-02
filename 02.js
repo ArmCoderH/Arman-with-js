91820913c7a7decacfaad66b208dba045c102319
Arman Hingora <armanhingora555@gmail.com>
2024-12-02 13:40:29 +0530

    js

---
+++ 02.js
@@ -0,0 +1,14 @@
+//here discuss with datatypes
+
+let numbers = null;
+let name  = "arman";
+console.log(typeof numbers);
+console.log(typeof name);
+
+/*  1.number,
+    2.string
+    3.object
+    4.symbol
+    5.null ->o/p is object
+    6.undefined -> o/p is undefined
+*/
---
+++ 03.js
@@ -0,0 +1,33 @@
+//in this tab convartion of datatype
+
+//let name = "arman"//this is string
+//let score = false
+//let name = "arman"
+//let name = "arman"
+//console.log(typeof name);
+
+// let valueinNumber = Number(score)
+// console.log(typeof valueinNumber);
+// console.log(valueinNumber);
+
+let score = 1
+//console.log(score);
+
+let scoreINOne = Boolean(score)
+console.log(typeof scoreINOne);
+
+console.log(scoreINOne);
+
+ 
+
+
+
+/*
+
+1) "33" => NaN
+2) "33abc" => NaN
+3) null => 0
+4) undefined => NaN
+5) true => 1 && false => 0
+
+*/
