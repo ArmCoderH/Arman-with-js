38b7dbc51c1c12b1114c11b737dfeb5a58a2e4c1
Arman Hingora <armanhingora555@gmail.com>
2024-12-04 21:50:21 +0530

    js

---
+++ 05.js
@@ -0,0 +1,19 @@
+//here comparision operator are used
+
+// console.log("2">1);
+// console.log(2>1);
+// console.log("2">"1");
+
+// console.log(null>0);
+// console.log(null<0);
+// console.log(null>=0);
+// console.log(null<=0);
+
+console.log(2==2);
+console.log("2"===2); //<-check data type
+
+
+
+/*note :- == only compare with two number without datatype
+          === campare with dataype  
+*/ 
