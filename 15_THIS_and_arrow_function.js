aa5329937c554d4d3cf718f703a5ca7115f3c411
Arman Hingora <armanhingora555@gmail.com>
2025-01-05 21:02:56 +0530

    js

--- 15_THIS_and_arrow_function.js
+++ 15_THIS_and_arrow_function.js
@@ -1,26 +1,29 @@
+//this keyword is only used in object not used in function
+
+
 const user = {
     username : "hitesh",
     price : 999,
 
     welcomemsg : function()
     {
-        console.log(`${this.price} , welcome to website`);
-        console.log(`${this.username} , welcome to website`);
+        // console.log(`${this.price} , welcome to website`);
+        // console.log(`${this.username} , welcome to website`);
     }
 }
 
 
 
-user.welcomemsg()
-user.username = "arman"
-user.welcomemsg()
+// user.welcomemsg()
+// user.username = "arman"
+//user.welcomemsg()
 
-console.log(this);
+//console.log(this);
 
 
 function chai()
 {
-    console.log(this);
+    //console.log(this);
     
 }
 
@@ -31,11 +34,49 @@ if (true) {
     const id = 321;
     if(true)
     {
-        console.log(id);
+        // console.log(id);
         
     }
-    console.log(id);
+    // console.log(id);
 }
 //console.log(id);
 
+
+
+//arrow function are used :->
+
+//orignal function syntex 
+
+
+
+// const hello1 = function ()
+// {
+//     let username = "arman";
+//     console.log(this);  
+// }
+
+// hello1()
+
+
+// const hello = () => 
+// {
+//     let username = "arman";
+//     console.log(this);  
+// }
+
+// hello()
+
+
+//explicit return
+const addTwo = (num1,num2) => {
+ return num1 + num2
+}
+
+console.log(addTwo(3,5))
+
+
+//implicit return 
+const divedTwo = (num1,num2) => num1 / num2
+
+console.log(divedTwo(15,5))
 
