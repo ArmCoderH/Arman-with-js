227e5d9d4428cb7703bf94311a014d5fad4a5060
Arman Hingora <armanhingora555@gmail.com>
2025-01-06 20:48:01 +0530

    js

---
+++ 19_control_flow(switch_case).js
@@ -0,0 +1,78 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+// let month = 3;
+
+// switch(month)
+// {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("april");
+//         break;
+//     default:
+//         console.log("exit");
+//         break;
+// }
+
+
+
+
+
+
+
+// let month = "march";
+
+// switch(month)
+// {
+//     case "jan":
+//         console.log("january");
+//         break;
+//     case "feb":
+//         console.log("feb");
+//         break;
+//     case "march":
+//         console.log("march");
+//         break;
+//     case "april":
+//         console.log("april");
+//         break;
+//     default:
+//         console.log("exit");
+//         break;
+// }
+
+
+
+
+
+//nullish coalescing operator (??) : null undefined
+
+let val1;
+
+//this operator used when api response is given at that time server side guve two values that time how to manage this
+
+val1 = 5 ?? 10
+val2 = null ?? 10
+val3 = undefined ?? 15
+val4 = null ?? 15 ?? 60
+
+console.log(val1);
+console.log(val2);
+console.log(val3);
+console.log(val4);
