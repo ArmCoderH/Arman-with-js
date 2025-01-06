95c810c70b99e1916a5506eb9193a1dadfc6375b
Arman Hingora <armanhingora555@gmail.com>
2025-01-06 20:48:15 +0530

    js

---
+++ 21_for_loop.js
@@ -0,0 +1,60 @@
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+
+//print table
+for (let i = 0; i <= 10; i++){
+   // console.log("Namste Duniya");
+}
+
+//nested for loop
+
+for(let i = 1; i<=50; i++)
+{
+    for(let j=1; j<=10; j++)
+    {
+        if(i==21)
+        {
+            //console.log(`${i} * ${j} = ${i*j}`);
+        }
+    }
+    //console.log("\n");
+}
+
+
+let myArray = ["flash", "batman", "superman"]
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    //console.log(element);
+}
+
+
+
+
+//berak and continue
+
+for (let i = 0; i < 10; i++) {
+
+    const element = i;
+    if (element==5) {
+       // console.log("5 is present");
+        break;
+    }
+
+    //console.log(i);
+}
+
+
+for (let i = 1; i <= 10; i++) {
+
+    const element = i;
+    if (element==5) {
+        console.log("5 is present");
+        continue;
+    }
+
+    console.log(i);
+}
