4fa1c8479edabaec8bd6ca445aae3caa2b820c03
Arman Hingora <armanhingora555@gmail.com>
2025-01-06 20:48:08 +0530

    js

---
+++ 20_control_flow(truthy&&falsy).js
@@ -0,0 +1,33 @@
+// falsy values
+
+
+// false , 0 , -0, BigInt 0n, "", null, undefined, NaN
+
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+
+
+
+let hii = function(){};
+let check = 'false'
+let check1 = false 
+
+if(check1)
+{
+    console.log("true");
+}
+else
+{
+    console.log("false");
+    
+}
+
+
+//torenory operator
+
+
+const iceTePrice  = 100
+iceTePrice >=80 ? console.log("true") : console.log("false");
+;
