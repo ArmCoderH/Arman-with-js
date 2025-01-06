2388a1b294f0a6652313fa0f244fd9c393c26348
Arman Hingora <armanhingora555@gmail.com>
2025-01-06 20:47:50 +0530

    js

---
+++ 18_control_flow(if_else).js
@@ -0,0 +1,124 @@
+//control flow in javascrip
+
+
+//1.If condition
+//2.if-else condition
+//3.nested if
+//4.switch case 
+
+
+
+// if(true)
+// {
+//     console.log("logged in sucssefully");
+    
+// }
+
+
+const mark = 500;
+
+//used assignmwent operator like :- ==,<,><=,>=,!=,===,!==
+
+// if(mark == 50)
+// {
+//     console.log("marks is 50");
+    
+// }
+// else
+// {
+//     console.log("marks is not 50");
+    
+// }
+
+
+
+//nested if
+
+
+// let maths = 50;
+// let english = 50;
+// let hindi = 10;
+
+// if(maths >= 50)
+// {
+//     console.log("you are passed in maths");
+    
+//     if(english >=50)
+//     {
+//         console.log("you are passed in english"); 
+//         if(hindi >= 50)
+//         {
+//             console.log("you are passed in hindi");
+//         }
+//         else
+//         {
+//             console.log("you are failed in hindi");
+//         }
+//     }
+//     else
+//     {
+//         console.log("you are failed in english");
+//     }
+// }
+// else
+// {
+//     console.log("you are failed in maths");
+// }
+
+
+
+
+// if(mark == 50)
+// {
+//     console.log("mark is 50");
+// }
+// else if (mark < 50) {
+//     console.log("mark is less then 50");
+    
+// }
+// else if(mark > 50)
+// {
+//     console.log("mark is gerater then 50");
+// }
+// else
+// {
+//     console.log("mark is not valid"); 
+// }
+
+
+
+//used AND and OR 
+
+// const loggedIN = true;
+// const logout = true;
+
+// if (loggedIN && logout) {
+//     console.log("true");
+// }
+// else
+// {
+//     console.log("false"); 
+// }
+
+
+// const loggedIN = false;
+// const logout = true;
+
+// if (loggedIN || logout) {
+//     console.log("true");
+// }
+// else
+// {
+//     console.log("false"); 
+// }
+
+
+
+//implicit return
+
+
+// if (true) console.log("yes"),console.log("yes2"); <<<<<<<<<<<<<--------this is bad habit not proper
+
+
+
+
