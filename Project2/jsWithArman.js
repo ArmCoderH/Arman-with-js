466a6e7843b7a083c3364b83fffd384cc67f3fbb
Arman Hingora <armanhingora555@gmail.com>
2025-01-08 21:33:58 +0530

    p2

---
+++ Project2/jsWithArman.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit',function (e){
+    e.preventDefault();
+
+
+const height = parseInt(document.querySelector('#height').value)
+const weight = parseInt(document.querySelector('#weight').value)
+const result = document.querySelector('#results')
+
+if (height === '' || height < 0 || isNaN(height)) {
+    result.innerHTML = `Plz enter a valid number ${height}`
+}
+else if (weight === '' || weight < 0 || isNaN(weight)) {
+    result.innerHTML = `Plz enter a valid number ${weight}`
+}
+else
+{
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+   // result.innerHTML = `<span>${bmi}</span>`
+
+    if (bmi <= 18.6) {
+        result.innerHTML = `your bmi value is = <span>${bmi}</span> : Under Weight`
+    } else if(bmi >= 18.6 && bmi <=24.9) {
+        result.innerHTML = `your bmi value is = <span>${bmi}</span> : Normal Weight`
+    }
+    else if (bmi >= 24.9) {
+        result.innerHTML = `your bmi value is = <span>${bmi}</span> : Over Weight`
+    }
+}
+
+
+});
